d1974661370cc70a64eb7b852042571ecddf4ac3
lzzze987 <166441979+lzzze987@users.noreply.github.com>
2024-04-09 01:10:50 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main() {
+    char s[20];
+    int state = 0, i = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", s);
+
+    while (s[i] != '\0') {
+        switch (state) {
+            case 0:
+                if (s[i] == 'a') state = 1;
+                else if (s[i] == 'b') state = 2;
+                else state = 6;
+                break;
+            case 1:
+                if (s[i] == 'a') state = 3;
+                else if (s[i] == 'b') state = 4;
+                else state = 6;
+                break;
+            case 2:
+                if (s[i] == 'b') state = 2;
+                else state = 6;
+                break;
+            case 3:
+                if (s[i] == 'a') state = 3;
+                else if (s[i] == 'b') state = 2;
+                else state = 6;
+                break;
+            case 4:
+                if (s[i] == 'b') state = 5;
+                else state = 6;
+                break;
+            case 5:
+                if (s[i] == 'b') state = 4;
+                else state = 6;
+                break;
+            default:
+                printf("\n%s is not recognized.\n", s);
+                return 0;
+        }
+        i++;
+    }
+
+    if (state == 1) printf("\n%s is accepted under rule 'a*'\n", s);
+    else if (state == 2 || state == 4) printf("\n%s is accepted under rule 'a*b+'\n", s);
+    else if (state == 5) printf("\n%s is accepted under rule 'abb'\n", s);
+
+    return 0;
+}
